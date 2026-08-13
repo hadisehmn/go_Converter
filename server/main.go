862fc99d234f2e4fdfa521ba43788be94da11613
hadisak <hadisehmanouchehri@gmail.com>
2026-08-13 03:36:20 +0330

    clean up unused code

--- server/main.go
+++ server/main.go
@@ -14,18 +14,11 @@ func main() {
 	convertController := user.NewConvertController(convertService)
 	http.HandleFunc("/convert", convertController.ConvertFile)
 
-	http.Handle(
-		"/downloads/", http.StripPrefix(
-			"/downloads/", http.FileServer(http.Dir("./downloads")),
-		),
-	)
-
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Server is healthy"))
 	})
 
-	converter.StartCleanupJob()
 	log.Println("Server is running on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
